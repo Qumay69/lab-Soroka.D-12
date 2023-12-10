ae3dea7429c3d4845f2c1f48748252212e4917af
Sorok <Sorok@DESKTOP-C9GGTD7>
2023-12-10 20:44:15 +0200

    Добавьте файлы проекта.

---
+++ lab Soroka.D 12/Program.cs
@@ -0,0 +1,31 @@
+﻿//Вариант 9 Уровень сложности средний
+//bool Simm(string S)
+//{
+//    if (S.Length <= 1)
+//    {
+//        return true;
+//    }
+//    else if (S[0] == S[S.Length - 1])
+//    {
+//        return Simm(S.Substring(1, S.Length - 2));
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
+
+//string[] Rick = new string[5] {"radar" , "levle" , "deified" , "hello" , "world"};
+
+//foreach (string R in Rick) 
+//{
+//    if (Simm(R))
+//    {
+//        Console.WriteLine($"{R} является симметричной");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{R} не является симметричной");
+//    }
+//}
+
